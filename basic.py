26b6b7356a4c8a977441bd70d553f91f47861a93
Sahil Khare <sahilkhare1997@gmail.com>
2020-04-06 16:23:29 +0530

    testing strategy file with data cache

--- basic.py
+++ basic.py
@@ -82,7 +82,7 @@ def get_analytics_data(scripname,startdate,enddate):
             output["close_min_90_perc"] = close_min_90_perc
             output["close_min_50_perc"] = close_min_50_perc
             output["close_min_10_perc"] = close_min_10_perc
-            time.sleep(2)
+            #time.sleep(2)
             print("Analytics data retrieved successfully for:" + scripname)
             run=1
         except:
@@ -92,25 +92,7 @@ def get_analytics_data(scripname,startdate,enddate):
         
     return output
 
-def get_nth_percentile(scripname,n,startdate,enddate):
-    print("getting analytics data for scrip: "+scripname)
-    run=0
-    attempt=1
-    while (not(run)):
-        try:
-            print("attempt="+str(attempt))
-            if(attempt>100):
-                break
-            data = get_history(symbol=scripname, start=startdate, end=enddate)
-            close_n_perc = np.percentile(data['Close'],n)
-            time.sleep(2)
-            print("Nth percentile data retrieved successfully for:" + scripname)
-            run=1
-        except:
-            run=0
-            print("Nth percentile retrieval unsuccessful...Trying again")
-            attempt=attempt+1
-    return close_n_perc  
+
 
 def get_historical_data(scripname,startdate,enddate):
     print("getting historical data for scrip: "+scripname)
@@ -133,22 +115,20 @@ def get_historical_data(scripname,startdate,enddate):
 
     
 
-def buy_sell_matrix(scripname,startdate,enddate):
-    buy = strategy_buy(scripname,startdate)
-    sell = strategy_sell(scripname,startdate)
-    data = get_historical_data(scripname,startdate,enddate)
+def buy_sell_matrix(scripname,startdate,enddate,data_whole):
+    
+    buy = strategy_buy(scripname,startdate,data_whole)
+    sell = strategy_sell(scripname,startdate,data_whole)
+    data = data_whole.loc[startdate:enddate]
     output = pd.DataFrame(columns=['buy','sell','CP'])
     for index,each_date_data in data.iterrows():
-        buy = strategy_buy(scripname,index)
-        sell = strategy_sell(scripname,index)
-        print(each_date_data)
-        print(index)
-        print(type(output))
+        buy = strategy_buy(scripname,index,data_whole)
+        sell = strategy_sell(scripname,index,data_whole)
         output.at[index,'CP'] = each_date_data["Close"]
         if (each_date_data["Close"]<buy):
             output.at[index,'buy'] = 1            
         else:
-            output.loc[index,'buy'] = 0
+            output.at[index,'buy'] = 0
         if (each_date_data["Close"]>sell):
             output.at[index,'sell'] = 1
         else:
@@ -159,20 +139,43 @@ def get_profit_loss(data,account_start=1000):
     in_position = 0
     account=account_start
     current_value = account
+    output = pd.DataFrame(columns=['transaction','CP'])
     for index,each_date_data in data.iterrows():
         if (each_date_data['buy']==1 and account -  each_date_data['CP'] > 0):
             account = account -  each_date_data['CP']
-            print(index)
-            print("buy")
+            output.at[index,'transaction'] = "buy"
+            output.at[index,'CP'] = each_date_data['CP']
             in_position =in_position +1
         elif(in_position>0 and each_date_data['sell']==1):
             account = account + each_date_data['CP']
             in_position =in_position -1
-            print(index)
-            print("sell")
+            output.at[index,'transaction'] = "sell"
+            output.at[index,'CP'] = each_date_data['CP']            
         current_value = each_date_data['CP']*in_position+account
         print("cuurentval = "+str(current_value))
-    return account,in_position,current_value,current_value- account_start 
+    return account,in_position,current_value,current_value- account_start ,output
+
+def get_nth_percentile(n,data):
+    close_n_perc = np.percentile(data['Close'],n)
+    return close_n_perc  
+
+
+
+def data_required(scripname,startdate,enddate):
+    return (get_historical_data(scripname,startdate+timedelta(-10),enddate))
+
+
+def strategy_buy(scripname,date,data_whole):
+    startdate = date + timedelta(-10)
+    enddate = date
+    data = data_whole.loc[startdate:enddate]
+    return get_nth_percentile(10,data)
+
+def strategy_sell(scripname,date,data_whole):
+    startdate = date + timedelta(-10)
+    enddate = date
+    data = data_whole.loc[startdate:enddate]
+    return get_nth_percentile(70,data)
 
 scripname="SBIN"
 startdate='2019-01-01'
@@ -180,21 +183,17 @@ start = datetime.strptime(startdate, '%Y-%m-%d')
 enddate='2020-01-01'
 end = datetime.strptime(enddate, '%Y-%m-%d')
 
-d = get_historical_data("SBIN",start,end)    
-data1 = buy_sell_matrix(scripname,start,end)         
-pl = get_profit_loss(data1)
+    
+data = data_required("SBIN",start,end)
+test = data.loc[datetime.date(start):datetime.date(end)]
 
-def strategy_buy(scripname,date):
-    startdate = date + timedelta(-10)
-    enddate = date
-    return get_nth_percentile(scripname,10,startdate,enddate)
+data1 = buy_sell_matrix(scripname,datetime.date(start),datetime.date(end),data)         
+account_cash,stocks_help,value_stock,value,buy_sell_timeframe = get_profit_loss(data1)
 
-cp = strategy_buy("SBIN",end)
 
-def strategy_sell(scripname,date):
-    startdate = date + timedelta(-10)
-    enddate = date
-    return get_nth_percentile(scripname,60,startdate,enddate)
+
+cp = strategy_buy("SBIN",end,data)
+
 
 
     
